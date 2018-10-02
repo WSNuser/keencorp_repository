79500baa2ed2df33e564a147ade525688cffa858
Mark Mooij <mmooij@keencorp.com>
2018-10-02 15:29:18 +0200

    Update

--- reporter/worker.py
+++ reporter/worker.py
@@ -2,7 +2,8 @@ import KeenCorpReporter
 
 credentials = {
    "api_access": {"username": "keencorp@keencorp", "password": "k33nc0rp"},
-   "advanced_stats": True
+   "advanced_stats": True,
+   "no_smoothing": True
 }
 
 kcr = KeenCorpReporter.KeenCorpReporter(credentials)
